faff81fa39ed3f29b1449d6d277504fe2b236ab0
tanay2000614 <tanaym2006@gmail.com>
2026-08-02 12:54:03 +0530

    Add files via upload

---
+++ week 1 java files/Q24/Q3.java
@@ -0,0 +1,8 @@
+public class Q3 {
+    public static void main(String[] args) throws Exception {
+        int total = 12;
+        int count = 1;
+        System.out.println(total / count);
+    
+    }
+}
